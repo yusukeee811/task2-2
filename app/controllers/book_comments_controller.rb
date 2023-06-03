d096b44608148b6871030cf87711c51b705c3a0c
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-06-03 11:59:16 +0000

    [Update]フォロー機能追加モデル作成まで

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,7 +8,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComent.find(params[:id]).destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_back(fallback_location: root_path)
   end
 
