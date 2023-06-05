bf489b9a274ed07f95463064c9673855b954d8a9
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-06-05 06:14:56 +0000

    [Update]フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-       resource :favorites, only: [:create, :destroy]
-       resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
  end
