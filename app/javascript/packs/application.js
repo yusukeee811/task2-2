ca2a0e022ca592f5cab33993effd6624df998a2e
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-06-06 15:42:40 +0000

    [Start]非同期通信実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,11 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require jquery
+//= require rails-ujs
