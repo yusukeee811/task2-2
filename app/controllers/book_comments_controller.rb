86641dd58199c6d2c0939f93fbfffba8e8182624
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-06-07 05:31:12 +0000

    [Fix]コメント非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = book.id
-    @comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+    end
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
-    @comment = BookComment.find(params[:id])
-    @comment.destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
